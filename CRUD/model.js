7ec0e10bc83f9d5f9d657f1ad58f5764a8d94095
Pratham Gadhiya <prathamgadhiya@gmail.com>
2024-11-27 09:27:14 +0530

    Node

--- CRUD/model.js
+++ CRUD/model.js
@@ -1,12 +1,12 @@
 const mongoose=require('mongoose');
 
-const studentschema=new mongoose.Schema({
+const studentSchema=new mongoose.Schema({
     name:String,
     age:Number,
     course:String,
     fees:Number,
 });
 
-const student=mongoose.model("student",studentschema)
+const student=mongoose.model("student",studentSchema)
 
 module.exports=student;
