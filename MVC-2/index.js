60d76effb91cff7a275d6ebbb562117ca737fc4e
Pratham Gadhiya <prathamgadhiya@gmail.com>
2024-11-28 11:07:07 +0530

    Node

--- MVC-2/index.js
+++ MVC-2/index.js
@@ -1,17 +1,18 @@
 const express = require('express')
 const dbconnection = require('./config/db.js');
 const productRoute = require("./routes/product.routes.js")
-
+const path=require("path");
+require("dotenv").config();
 const PORT = process.env.PORT || 8080;
 const app = express()
-
+app.use(express.urlencoded({extended:true}));
 
 app.use(express.json())
-app.use("/products", productRoute)
-app.get("/", (req, res) => {
-    res.send("Welcome to the server");
-})
 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname,"view/form.html"));
+})
+app.use("/products", productRoute);
 
 
 app.listen(PORT, () => {
