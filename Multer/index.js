8d6d8990b9540f1d3466075d3fb32a44ff778f6e
Pratham Gadhiya <prathamgadhiya@gmail.com>
2024-11-29 13:03:19 +0530

    Node

--- Multer/index.js
+++ Multer/index.js
@@ -3,16 +3,16 @@ const dbconnection = require('./config/db.js');
 const productRoute = require("./routes/product.routes.js")
 const path=require("path");
 require("dotenv").config();
-const PORT = process.env.PORT || 8090;
 const app = express()
-app.use(express.urlencoded({extended:true}));
-
 app.use(express.json())
+app.use(express.urlencoded({extended:true}));
 app.use("/uploads",express.static(path.join(__dirname,"uploads")));
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname,"view/form.html"));
 })
 app.use("/products", productRoute);
+
+const PORT = process.env.PORT || 8090;
 
 
 app.listen(PORT, () => {
