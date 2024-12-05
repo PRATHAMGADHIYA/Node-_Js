a94c7c7d0be1251860292a1d17e2664846444453
Pratham Gadhiya <prathamgadhiya@gmail.com>
2024-12-05 11:04:58 +0530

    Node

--- PR– 3 CONVERT HTML TO NODE JS/recipe/routes/recipe.route.js
+++ PR– 3 CONVERT HTML TO NODE JS/recipe/routes/recipe.route.js
@@ -1,26 +1,9 @@
-const express = require('express');
-const router = express.Router();
-const recipeController = require('../controller/recipe.controller');
+const {Router}=require("express");
+const { createrecipe } = require("../controller/recipe.controller");
 
-// Base route
-router.get('/', recipeController.welcome);
+const recipeRoute = Router();
 
-// Recipe listing route
-router.get('/recipe/all', recipeController.getAllRecipes);
+recipeRoute.get("/recipe/all",createrecipe);
+recipeRoute.post
 
-// Add new recipe
-router.get('/add', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/recipe.html'));
-});
-router.post('/recipe/add', recipeController.addRecipe);
-
-// Update recipe
-router.patch('/recipe/update/:id', recipeController.updateRecipe);
-
-// Delete recipe
-router.delete('/recipe/delete/:id', recipeController.deleteRecipe);
-
-// Filter recipes
-router.get('/recipe/filter', recipeController.filterRecipes);
-
-module.exports = router;
+module.exports = recipeRoute;
